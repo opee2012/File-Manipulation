b291b6d45b75bb5c2b58b402a958e1380d45d399
Jacob Borth <borthj2@winthrop.edu>
2022-11-08 16:01:14 -0500

    Completed Add Company

--- admin.php
+++ admin.php
@@ -25,16 +25,19 @@
         }
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $comp_name = test_input($_POST["name"]);
+            $comp_name = test_input($_POST["comp_name"]);
+            $error = 0;
             // check if name only contains letters and whitespace
             if (!preg_match("/^[a-zA-Z-' ]*$/",$comp_name)) {
                 $nameErr = "Only letters and whitespace allowed";
+                $error = 1;
             }
 
-            $phone_num = test_input($_POST["phone"]);
+            $phone_num = test_input($_POST["phone_num"]);
             // check if name only contains letters and whitespace
             if (!preg_match("/1-[0-9]{3}-[0-9]{3}-[0-9]{4}/",$phone_num)) {
                 $numberErr = "Incorrect format ex. 1-###-###-####";
+                $error = 1;
             }
         }
         ?>
@@ -42,22 +45,36 @@
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         
     <h3>Add Entry</h3>
-    Company Name:
-        <label>
-            <input type="text" name="name" value="<?php echo $comp_name;?>">
-        </label>
-        <span class="error"><?php echo $nameErr;?></span><br>
-    Company Phone:
-        <label>
-            <input type="text" name="phone" value="<?php echo $phone_num;?>">
-        </label>
-        <span class="error"><?php echo $numberErr;?></span><br>
-    <input type="submit" name="submit" value="Submit"><br>
+        <TABLE>
+            <tr>
+                <td><LABEL for="add_comp">Company Name:</LABEL></td>
+                <td><INPUT type="text" id="add_comp" name="comp_name" value="<?php echo $comp_name;?>"></td>
+                <td><SPAN style="color: red;font-style: italic"><?php echo $nameErr;?></SPAN></td>
+            </tr>
+            <tr>
+                <td><LABEL for="add_phone">Company Phone:</LABEL></td>
+                <td><INPUT type="text" id="add_phone" name="phone_num" value="<?php echo $phone_num;?>"></td>
+                <td><SPAN style="color: red;font-style: italic"><?php echo $numberErr;?></SPAN></td>
+            </tr>
+        </TABLE>
+        <INPUT type="reset" value="Reset">
+        <INPUT type="submit" value="Submit">
+        <br><br>
         <?php
-        $list = array ($comp_name, $phone_num);
-        $file = fopen("callList.csv", "a");
-        fputcsv($file, $list);
-        fclose($file);
+        // sanitize the var $_POST['name'] with a basic filter
+        $A = array(
+            filter_input(INPUT_POST, 'comp_name'),
+            filter_input(INPUT_POST, 'phone_num')
+        );
+
+        if ($A[0] != '' && $A[1] != '' && $error == 0) {
+            $openFile = fopen('callList.csv', 'a+');
+
+            // append the sanitized input to our text file
+            $fp = fwrite($openFile, implode(',', $A). "\r\n");
+
+            fclose($openFile);
+        }
         ?>
     <h3>Remove Entry</h3>
         <?php
